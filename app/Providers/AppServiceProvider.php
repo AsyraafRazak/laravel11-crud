f36657b1853613eac2e194d83aee72c7e9d54e64
Asyraaf Razak <m.asyraaf.ra2ak@gmail.com>
2025-02-10 14:55:39 +0800

    routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\EmployeeRepository\employeeinterface;
+use App\EmployeeRepository\employeeimplementation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(employeeinterface::class,employeeimplementation::class);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post("addemployee", [EmployeeApiController::class, 'store',]);
+Route::get("getallemployee", [EmployeeApiController::class, 'getallemployee',]);
+Route::put("updateemployee/{id}", [EmployeeApiController::class, 'updateemployee',]);
+Route::delete("deleteemployee/{id}", [EmployeeApiController::class, 'deleteemployee',]);
